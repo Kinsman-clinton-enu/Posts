570b7143b5b3a4944301bede6224518ca0a8bd7a
Enu Clinton <enudariusgodwill@gmail.com>
2024-08-03 06:28:56 +0100

    installing middleware to solve synchronous issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers'; // Assuming you have your reducers imported
 import 'semantic-ui-css/semantic.min.css'
 
-
+const store = configureStore({reducer:reducers}, applyMiddleware(thunk));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Provider store={configureStore({reducer:reducers})}>
+        <Provider store={store}>
             <App />
         </Provider>
     </React.StrictMode>
