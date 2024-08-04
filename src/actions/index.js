6a10d9eb50b3e8d316c674e79c21986da4ae638a
Enu Clinton <enudariusgodwill@gmail.com>
2024-08-04 03:59:51 +0100

    setting up action creator for userheader

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,4 +4,10 @@ export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
      dispatch({type: 'FETCH_POSTS', playload: response });
    };
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`users/${id}`);
+
+    dispatch({type: 'FETCH_USER', playload: response.data})
+}
  
